9fb0cbb83fd78386a23cea03b7fbee8903e34830
Gabriel Cisneros <cisnerog@flip3.engr.oregonstate.edu>
2016-12-29 09:03:42 -0800

    added more functions

--- square.cpp
+++ square.cpp
@@ -2,4 +2,25 @@
 
 string square:: get_name(){
 	return name;
-};
+}
+
+void square:: set_name(string s){
+	name = s;
+}
+char square:: get_type(){
+	return type;
+}
+
+void square:: set_type(char t){
+	type = t;
+}
+
+class square* square:: get_next(){
+	return next;
+}
+
+void square:: set_next(class square * n){
+	next = n;
+}
+
+
--- square.h
+++ square.h
@@ -7,12 +7,17 @@
 class square{
 	private:
 		string name;
+		char type;
 		class player * players_on_square; //??
 		class square * next; //linked list
 	public:
-		virtual void action(class player * player_) = 0; //going to change
+		virtual void action(class player * player_) = 0;
 		string get_name();
-		
+		void set_name(string); //name of square
+		char get_type();
+		void set_type(char); //property, community...
+		class square * get_next();
+		void set_next(class square *);
 };
 
 #endif
